4c1e6c5b3f88a4d9a594081984c16a84ac5183af
Ura427 <urakost427@gmail.com>
2024-03-06 14:08:15 +0200

    component for gender radio element

---
+++ Authorization/src/components/GenderRadio.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+interface GenderRadioProps {
+    id: string,
+    value: string,
+    handleGenderChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+function GenderRadio({id, value, handleGenderChange}: GenderRadioProps) {
+  return (
+    <div className="w-1/3 py-1 px-2 flex flex-nowrap justify-between rounded-md bg-stone-100 ">
+        <label htmlFor={id} className="mr-4">
+          {value}
+        </label>
+        <input
+          id={id}
+          type="radio"
+          name="gender"
+          value={value}
+          onChange={handleGenderChange}
+        />
+      </div>
+  )
+}
+
+export default GenderRadio

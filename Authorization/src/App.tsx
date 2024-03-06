a17496260cca591dbe18245a036afeba7af24813
Ura427 <urakost427@gmail.com>
2024-03-06 14:04:54 +0200

    minor changes

--- Authorization/src/App.tsx
+++ Authorization/src/App.tsx
@@ -1,15 +1,13 @@
-// import { useState } from 'react'
-import './App.css'
-import Form from './components/Form'
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
-
-
   return (
-    <div className='registrationPage'>
-      <Form/>
+    // <div className="registrationPage">
+    <div className="flex justify-center items-center h-screen bg-stone-100">
+      <Form />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

5287966f2db9e67e753d894fd760611f3b95de31
Ura427 <urakost427@gmail.com>
2024-03-15 13:17:16 +0200

    component for taking user gender

--- Authorization/src/components/GenderRadio.tsx
+++ Authorization/src/components/GenderRadio.tsx
@@ -8,7 +8,19 @@ interface GenderRadioProps {
 
 function GenderRadio({id, value, handleGenderChange}: GenderRadioProps) {
   return (
-    <div className="w-1/3 py-1 px-2 flex flex-nowrap justify-between rounded-md bg-stone-100 ">
+    // <div className="w-1/3 py-1 px-2 flex flex-nowrap justify-between rounded-md bg-stone-100 ">
+    //     <label htmlFor={id} className="mr-4">
+    //       {value}
+    //     </label>
+    //     <input
+    //       id={id}
+    //       type="radio"
+    //       name="gender"
+    //       value={value}
+    //       onChange={handleGenderChange}
+    //     />
+    //   </div>
+    <div className="w-full py-1 px-2 flex flex-nowrap justify-between rounded-md bg-stone-100 ">
         <label htmlFor={id} className="mr-4">
           {value}
         </label>

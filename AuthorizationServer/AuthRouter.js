80374b8da3d9bd4d2ad28c29a4c740250785b2ba
Ura427 <urakost427@gmail.com>
2024-03-15 13:28:58 +0200

    router for auth

---
+++ AuthorizationServer/AuthRouter.js
@@ -0,0 +1,11 @@
+const Router = require("express");
+const router = new Router();
+const controller = require("./authController");
+const authMiddleware = require("./middleware/authMiddleware");
+const roleMiddleware = require("./middleware/roleMiddleware");
+
+router.post("/registration", controller.registration);
+router.post("/login", controller.login);
+router.get("/users", roleMiddleware(["ADMIN"]), authMiddleware, controller.getUsers);
+
+module.exports = router;

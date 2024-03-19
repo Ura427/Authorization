0ba17eab3d6cbab92da12cd8045f65193ed16a48
Ura427 <urakost427@gmail.com>
2024-03-19 21:16:04 +0200

    use redux store

--- Authorization/src/App.tsx
+++ Authorization/src/App.tsx
@@ -1,22 +1,29 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Form from "./components/Form";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
-import Home from "./pages/Home"
-import { useState } from "react";
+import Home from "./pages/Home";
+import { useSelector } from "react-redux";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [currUser, setCurrUser] = useState(null)
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  const currUser = useSelector((state) => state.user);
+
+console.log(isLoggedIn)
+console.log(currUser)
+console.log(useSelector((state) => state))
+
   return (
     <BrowserRouter>
       <div className="flex justify-center items-center h-screen bg-stone-100">
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/registration" element={<Registration />} />
-          <Route path="/home" element={<Home/>} />
-          {/* <Route path="/login" element={<Login />} /> */}
+          <Route
+            path="/home"
+            element={isLoggedIn ? <Home /> : <Navigate to="/" />}
+          />
         </Routes>
       </div>
     </BrowserRouter>

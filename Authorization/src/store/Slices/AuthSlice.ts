b4d787d3333cccda07518b9c86acf1e069d08b1d
Ura427 <urakost427@gmail.com>
2024-03-19 21:19:07 +0200

    Create slice for authorization

---
+++ Authorization/src/store/Slices/AuthSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoggedIn: false,
+  user: {
+    id: "",
+    roles: [],
+    username: ""
+  }
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    // Authorize: (state, action) => {
+    Authorize: (state, action) => {
+      state.isLoggedIn = true;
+      state.user = action.payload;
+    },
+    // Unauthorize: (state, action) => {
+    Unauthorize: (state) => {
+      state.isLoggedIn = false;
+      state.user = initialState.user
+    },
+  },
+});
+
+
+export const {Authorize, Unauthorize} = authSlice.actions
+
+export default authSlice.reducer;

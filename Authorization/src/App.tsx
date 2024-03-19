a3349d2dbc49e786ba8f82aa2960068de72a5b2a
Ura427 <urakost427@gmail.com>
2024-03-19 12:04:34 +0200

    add routing

--- Authorization/src/App.tsx
+++ Authorization/src/App.tsx
@@ -1,12 +1,25 @@
 import "./App.css";
-import Form from "./components/Form";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Form from "./components/Form";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import Home from "./pages/Home"
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currUser, setCurrUser] = useState(null)
   return (
-    // <div className="registrationPage">
-    <div className="flex justify-center items-center h-screen bg-stone-100">
-      <Form />
-    </div>
+    <BrowserRouter>
+      <div className="flex justify-center items-center h-screen bg-stone-100">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/home" element={<Home/>} />
+          {/* <Route path="/login" element={<Login />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 

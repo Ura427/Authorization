1854d518cf13ef9a14f8339d9f370686280516e9
Ura427 <urakost427@gmail.com>
2024-03-19 21:16:42 +0200

    add logout btn

--- Authorization/src/pages/Home.tsx
+++ Authorization/src/pages/Home.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {Unauthorize} from "../store/Slices/AuthSlice"
 
 function Home() {
+  const currUserName = useSelector((state) => state.user.username);
+  const dispatch = useDispatch()
+
+  const LogoutHandler = () => {
+      dispatch(Unauthorize())
+  }
+
   return (
-    <div>Home</div>
-  )
+    <div className="flex flex-col">
+      <span>Hello, {currUserName}</span>
+      <button className="py-1 px-2 mt-8 bg-cyan-500 rounded-md text-white " onClick={LogoutHandler}>
+        Logout
+      </button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;

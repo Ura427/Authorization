034b4819b8a2afa285531c34ff7e105cd34d98dc
Ura427 <urakost427@gmail.com>
2024-03-20 16:49:04 +0200

    return types

--- Authorization/src/store/store.ts
+++ Authorization/src/store/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./Slices/AuthSlice";
+import { useDispatch } from "react-redux";
 
 const store = configureStore({
     reducer: authReducer
 });
 
+export type RootType = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch: () => AppDispatch = useDispatch
 export default store
